996b8887af5e5aea9a82bf7ec22358d2e9bd47a7
Joakim Haugen <joakim.haugen@sintef.no>
2018-01-16 22:15:42 +0100

    STATIC SHARED world

--- conanfile.py
+++ conanfile.py
@@ -27,14 +27,13 @@ class LibharuConan(ConanFile):
                               '''project(libharu C)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
-        tools.replace_in_file("libharu/CMakeLists.txt", "if(BUILD_SHARED_LIBS)",
-        '''if(BUILD_SHARED_LIBS)
-  set(LIBHPDF_SHARED ON)
-  set(LIBHPDF_STATIC NO)
-else()
-  set(LIBHPDF_SHARED NO)
-  set(LIBHPDF_STATIC ON)
-endif()''')
+        tools.replace_in_file("libharu/CMakeLists.txt", 
+                              "set(LIBHPDF_SHARED ON)",
+                              '''set(LIBHPDF_SHARED ON)
+  set(LIBHPDF_STATIC OFF)
+else(BUILD_SHARED_LIBS)
+  set(LIBHPDF_SHARED OFF)
+  set(LIBHPDF_STATIC ON)''')
         tools.replace_in_file("libharu/CMakeLists.txt",
                               "cmake_minimum_required(VERSION 2.4.8 FATAL_ERROR)",
                               "cmake_minimum_required(VERSION 3.1.2)")
