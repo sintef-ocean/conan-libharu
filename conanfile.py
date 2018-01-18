4c9220289851fd54c5dfd96510295d63c9bec0c3
Joakim Haugen <joakim.haugen@gmail.com>
2018-01-19 00:25:56 +0100

    Still funky. fpic

--- conanfile.py
+++ conanfile.py
@@ -8,16 +8,16 @@ class LibharuConan(ConanFile):
     homepage = "http://libharu.org"
     author = "Joakim Haugen (joakim.haugen@gmail.com)"
     description = "libHaru is a free, cross platform, open source library for generating PDF files."
-    settings = "os", "compiler", "build_type", "arch", "os_build", "arch_build"
+    settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=True"
-    generators = "cmake"
+    generators = "cmake" ,
     requires = "zlib/[>=1.2.11]@conan/stable", "libpng/[>=1.6.34]@bincrafters/stable"
-    exports = "lib_license/LICENSE"
+    exports = "lib_license/LICENSE" , "FindLibharu.cmake"
 
     def requirements(self):
-        self.options["zlib"].shared = self.options.shared
-        self.options["libpng"].shared = self.options.shared
+        self.options["zlib"].shared = False
+        self.options["libpng"].shared = False
   
     def source(self):
 
@@ -36,7 +36,9 @@ else(BUILD_SHARED_LIBS)
   set(LIBHPDF_STATIC ON)''')
         tools.replace_in_file("libharu/CMakeLists.txt",
                               "cmake_minimum_required(VERSION 2.4.8 FATAL_ERROR)",
-                              "cmake_minimum_required(VERSION 3.1.2)")
+                              '''cmake_minimum_required(VERSION 3.1.2)
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+                              ''')
         tools.replace_in_file("libharu/CMakeLists.txt",
                               "set(LIBHPDF_MINOR 2)", "set(LIBHPDF_MINOR 3)")
         tools.replace_in_file("libharu/CMakeLists.txt", "set(CMAKE_MODULE_PATH", 
@@ -51,6 +53,7 @@ else(BUILD_SHARED_LIBS)
     def package(self):
         self.copy("lib_license/LICENSE", dst="licenses", src=self.source_folder,
                   ignore_case=True, keep_path=False)
+        self.copy("FindLibharu.cmake", dst=".", src=self.source_folder, keep_path=False)
 
     def package_info(self):
         if self.settings.os == "Windows":
