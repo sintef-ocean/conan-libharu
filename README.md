4d1a56ad9815d93ed078f9f91b96094dc99d63a8
Joakim Haugen <joakim.haugen@gmail.com>
2018-01-21 20:48:23 +0100

    shebang, TARGETS, cleaning up more, improved readme

--- README.md
+++ README.md
@@ -1,6 +1,63 @@
 [![Download](https://api.bintray.com/packages/joakimono/conan/libharu%3Ajoakimono/images/download.svg)](https://bintray.com/joakimono/conan/libharu%3Ajoakimono/_latestVersion)
 [![Build Status UNIX](https://travis-ci.org/joakimono/conan-libharu.png?branch=master)](https://travis-ci.org/joakimono/conan-libharu)
 [![Build Status WIND](https://ci.appveyor.com/api/projects/status/github/joakimono/conan-libharu?branch=master&svg=true)](https://ci.appveyor.com/project/joakimono/conan-libharu)
-# conan-libharu 
 
-libharu library package for conan.io
+
+[Conan.io](https://conan.io) recipe for [libharu](http://libharu.org).
+
+The recipe generates library packages, which can be found at [Bintray](https://bintray.com/joakimono/conan/libharu%3Ajoakimono).
+The package is usually consumed using the `conan install` command or a *conanfile.txt*.
+
+## How to use this package
+
+1. Add remote to conan's package [registry.txt](http://docs.conan.io/en/latest/reference/config_files/registry.txt.html):
+
+   ```bash
+   $ conan remote add joakimono https://api.bintray.com/conan/joakimono/conan
+   ```
+
+2. Using *conanfile.txt* in your project with *cmake*
+
+   Add a [*conanfile.txt*](http://docs.conan.io/en/latest/reference/conanfile_txt.html) to your project. This file describes dependencies and your configuration of choice, e.g.:
+
+   ```
+   [requires]
+   libharu/[>=2.3.0]@joakimono/stable
+
+   [options]
+   libharu:shared=False
+
+   [imports]
+   licenses, * -> ./licenses @ folder=True
+
+   [generators]
+   cmake
+   ```
+
+   Insert into your *CMakeLists.txt* something like the following lines:
+   ```cmake
+   cmake_minimum_required(VERSION 3.1.2)
+   project(TheProject CXX)
+
+   include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+   conan_basic_setup(TARGETS)
+
+   add_executable(the_executor code.cpp)
+   target_link_libraries(the_executor CONAN_PKG::libharu)
+   ```
+   Then, do
+   ```bash
+   $ mkdir build && cd build
+   $ conan install ..
+   ```
+   You can now continue with the usual dance with cmake commands for configuration and compilation. For details on how to use conan, please consult [Conan.io docs](http://docs.conan.io/en/latest/)
+
+## Package options
+
+Option | Default | Domain
+---|---|--- 
+shared|False|[True, False]
+
+## Known recipe issues
+
+None
--- conanfile.py
+++ conanfile.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from conans import ConanFile, CMake, tools
 
 class LibharuConan(ConanFile):
@@ -11,10 +14,12 @@ class LibharuConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=True"
-    generators = "cmake" ,
-    requires = "zlib/[>=1.2.11]@conan/stable", "libpng/[>=1.6.34]@bincrafters/stable"
-    exports = "lib_license/LICENSE" , "FindLibharu.cmake"
-
+    generators = "cmake"
+    requires = ("zlib/[>=1.2.11]@conan/stable", "libpng/[>=1.6.34]@bincrafters/stable")
+    exports = ["lib_license/LICENSE" , "FindLibharu.cmake"]
+    source_subfolder = "libharu"
+    build_subfolder = "build_subfolder"
+    
     def requirements(self):
         self.options["zlib"].shared = False
         self.options["libpng"].shared = False
@@ -22,29 +27,31 @@ class LibharuConan(ConanFile):
     def source(self):
         self.run("git clone --depth 1 -b RELEASE_2_3_0 https://github.com/libharu/libharu.git")
         
-        tools.replace_in_file("libharu/CMakeLists.txt", "project(libharu C)",
-                              '''project(libharu C)
-                              include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-                              conan_basic_setup()''')
-        tools.replace_in_file("libharu/CMakeLists.txt",
-                              "cmake_minimum_required(VERSION 2.4.8 FATAL_ERROR)",
+        tools.replace_in_file("{}/CMakeLists.txt".format(self.source_subfolder),
+                              "project(libharu C)",
                               '''cmake_minimum_required(VERSION 3.1.2)
-                              set(CMAKE_POSITION_INDEPENDENT_CODE ON)''')
-        tools.replace_in_file("libharu/CMakeLists.txt", 
+                              project(libharu C)
+                              include(${CMAKE_BINARY_DIR}/../conanbuildinfo.cmake)
+                              conan_basic_setup()''')
+        tools.replace_in_file("{}/CMakeLists.txt".format(self.source_subfolder),
+                              "cmake_minimum_required(VERSION 2.4.8 FATAL_ERROR)",
+                              '''set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)''')
+        tools.replace_in_file("{}/CMakeLists.txt".format(self.source_subfolder),
                               "set(LIBHPDF_SHARED ON)",
                               '''set(LIBHPDF_SHARED ON)
                                 set(LIBHPDF_STATIC OFF)
                               else(BUILD_SHARED_LIBS)
                                 set(LIBHPDF_SHARED OFF)
                                 set(LIBHPDF_STATIC ON)''')
-        tools.replace_in_file("libharu/CMakeLists.txt",
+        tools.replace_in_file("{}/CMakeLists.txt".format(self.source_subfolder),
                               "set(LIBHPDF_MINOR 2)", "set(LIBHPDF_MINOR 3)")
-        tools.replace_in_file("libharu/CMakeLists.txt", "set(CMAKE_MODULE_PATH", 
+        tools.replace_in_file("{}/CMakeLists.txt".format(self.source_subfolder),
+                              "set(CMAKE_MODULE_PATH", 
                               "set(CMAKE_MODULE_PATH ${CONAN_LIBPNG_ROOT}")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="libharu")         
+        cmake.configure(source_folder=self.source_subfolder, build_folder=self.build_subfolder)         
         cmake.build()
         cmake.install()
 
@@ -54,15 +61,17 @@ class LibharuConan(ConanFile):
         self.copy("FindLibharu.cmake", dst=".", src=self.source_folder, keep_path=False)
 
     def package_info(self):
-        if self.settings.os == "Windows":
+        if self.settings.compiler == "Visual Studio":
             self.cpp_info.libs = ['libhpdf']
         else:
             self.cpp_info.libs = ['hpdf']
         if not self.options.shared:
             self.cpp_info.libs[0] += 's'
-        if self.settings.os == "Windows":
+        if self.settings.compiler == "Visual Studio":
             if self.settings.build_type == "Debug":
                 self.cpp_info.libs[0] += 'd'
+            if self.settings.options.shared:
+                self.cpp_info.defines = ["HPDF_DLL"] # mingw/cygwin also?
 
     def configure(self):
         del self.settings.compiler.libcxx
